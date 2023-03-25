3c819bdc536d537359e9b87b6b1cc9b4f8ea12f8
JFelipe115 <netsoft00@gmail.com>
2023-03-25 17:38:31 -0500

    Category Edit

--- Sales.Shared/Entities/Category.cs
+++ Sales.Shared/Entities/Category.cs
@@ -8,9 +8,9 @@ namespace Sales.Shared.Entities
     {
         public int Id { get; set; }
 
-        [Display(Name = "Categoria")]
+        [Display(Name = "Categoría")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
         public string Name { get; set; } = null!;
     }
 }
